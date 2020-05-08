7c70e663a2cbdd521f9570a71a46b1251e1fd1bf
bogdanpoh <bogdan20205@gmail.com>
2020-05-09 00:47:28 +0300

    Add design for Converter Tab

--- Currencies/Controller/ConverterViewController.swift
+++ Currencies/Controller/ConverterViewController.swift
@@ -13,60 +13,105 @@ enum ButtonState {
 }
 
 class ConverterViewController: UIViewController {
-
-    @IBOutlet weak var deleteButton: UIButton!
     
+    //MARK: - Outlets
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var changeButton: UIButton!
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var subButton: UIButton!
+    @IBOutlet weak var equalityButton: UIButton!
+    
+    @IBOutlet weak var firstLabel: UILabel!
+    @IBOutlet weak var secondLabel: UILabel!
+    
+    @IBOutlet weak var firstSelectCurrencyButton: UIButton!
+    @IBOutlet weak var secondSelectCurrencyButton: UIButton!
+    
+    
+    //MARK: - Properties
+    var selectLabel: UILabel!
+    
+    
+    //MARK: - Functions
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
-    }
-    
-    @IBAction func downButtonTapped(_ sender: UIButton) {
-        changeStateButton(sender, .downTapped)
-    }
-    
-    
-    @IBAction func upInsideButtonTapped(_ sender: UIButton) {
-        changeStateButton(sender, .upTapped)
+        selectLabel = firstLabel
+        
+        let deleteButtonLongTapped = UILongPressGestureRecognizer(target: self, action: #selector(longTapped))
+        self.deleteButton.addGestureRecognizer(deleteButtonLongTapped)
     }
     
     func changeStateButton(_ button: UIButton, _ state: ButtonState) {
         
-        var buttonImageName = ""
-        let buttonControlState: UIControl.State = getCurrentControlState(state)
+        var downColor: UIColor!
         
         switch button {
-        case deleteButton:
-            buttonImageName = "delete"
+        case changeButton:
+            downColor = .systemYellow
+            
+        case addButton:
+            downColor = .systemGreen
+            
+        case subButton:
+            downColor = .systemRed
+            
+        case equalityButton:
+            downColor = .systemBlue
+            
         default:
-            break
+            downColor = UIColor(red: 240/255, green: 240/255, blue: 240/255, alpha: 1)
         }
         
-        if let name = getButtonImage(buttonImageName, state) {
-            button.setImage(name, for: buttonControlState)
-        }
+        setButtonImage(button, state)
         
-        switch state {
-        case .upTapped:
-            button.backgroundColor = .white
-        case .downTapped:
-            button.backgroundColor = .lightGray
-        }
+        changeBackgroundColorButton(button, state, downTappedColor: downColor)
+        
     }
     
-    func getButtonImage(_ nameImage: String, _ state: ButtonState) -> UIImage? {
+    func setButtonImage(_ button: UIButton, _ state: ButtonState) {
         
-        var name = ""
+        var imageName: String?
+        let buttonControlState: UIControl.State = getCurrentControlState(state)
         
         switch state {
         case .upTapped:
-            name = nameImage + "-outline"
+            switch button {
+            case deleteButton:
+                imageName = "delete-outline"
+            case changeButton:
+                imageName = "change"
+            case addButton:
+                imageName = "add"
+            case subButton:
+                imageName = "sub"
+            case equalityButton:
+                imageName = "equality"
+            default:
+                break
+            }
         case .downTapped:
-            name = nameImage + "-fill"
+            switch button {
+            case deleteButton:
+                imageName = "delete-fill"
+            case changeButton:
+                imageName = "change-select"
+            case addButton:
+                imageName = "add-select"
+            case subButton:
+                imageName = "sub-select"
+            case equalityButton:
+                imageName = "equality-select"
+            default:
+                break
+            }
         }
         
-        return UIImage(named: name, in: nil, with: nil)
+        if let name = imageName {
+            if let image = UIImage(named: name) {
+                button.setImage(image, for: buttonControlState)
+            }
+        }
     }
     
     func getCurrentControlState(_ state: ButtonState) -> UIControl.State {
@@ -75,6 +120,85 @@ class ConverterViewController: UIViewController {
             return .normal
         case .downTapped:
             return .highlighted
+        }
+    }
+    
+    func changeBackgroundColorButton(_ button: UIButton, _ state: ButtonState, upTappedColor: UIColor = .white, downTappedColor: UIColor) {
+        switch state {
+        case .upTapped:
+            button.backgroundColor = upTappedColor
+        case .downTapped:
+            button.backgroundColor = downTappedColor
+        }
+    }
+    
+    func changeLabel(currentLabel: UILabel) {
+        
+        if currentLabel == firstLabel {
+            selectLabel = secondLabel
+        } else if currentLabel == secondLabel {
+            selectLabel = firstLabel
+        }
+        
+    }
+    
+    func inputNumberToLabel(_ text: String) {
+        if var selectLabelText = selectLabel.text {
+            
+            if selectLabelText.first == "0" {
+                selectLabelText.removeFirst()
+            }
+            
+            guard let symbol = selectLabelText.last else { return }
+            
+            selectLabelText.removeLast()
+            
+            selectLabel.text = "\(selectLabelText)\(text)\(symbol)"
+        }
+    }
+    
+    func removeLastInLabel() {
+        if var selectLabelText = selectLabel.text {
+        
+            guard let symbol = selectLabel.text?.last else { return }
+            let result = selectLabelText.replacingOccurrences(of: String(describing: symbol), with: "")
+            selectLabelText = result
+            selectLabelText.removeLast()
+            
+            if selectLabelText.count >= 1 {
+                selectLabel.text = "\(selectLabelText)\(symbol)"
+            } else {
+                selectLabel.text = "0\(symbol)"
+            }
+            
+        }
+    }
+    
+    //MARK: - Actions
+    @IBAction func downButtonTapped(_ sender: UIButton) {
+        changeStateButton(sender, .downTapped)
+    }
+    
+    
+    @IBAction func upInsideButtonTapped(_ sender: UIButton) {
+        changeStateButton(sender, .upTapped)
+        
+        if let value = sender.titleLabel?.text {
+            inputNumberToLabel(value)
+        } else {
+            switch sender {
+            case deleteButton:
+                removeLastInLabel()
+            default:
+                break
+            }
+        }
+    }
+    
+    @objc func longTapped(gesture: UILongPressGestureRecognizer) {
+        if gesture.state == UIGestureRecognizer.State.began {
+            print("Long pressed")
+            deleteButton.backgroundColor = .white
         }
     }
 }
